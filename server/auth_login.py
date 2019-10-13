abf7c61860d61163aa4e1c19d71295121dab80ee
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-13 13:35:37 +1100

    started implementing register and login

--- server/auth_login.py
+++ server/auth_login.py
@@ -1,6 +1,8 @@
 ##auth_login definition
 import re
-from email_check import *
+from email_check import email_check
+# import the function and the global dictionary
+import auth_register
 
 '''
 Given a registered users' email and password and generates a valid token for the user to remain authenticated
@@ -10,15 +12,34 @@ Email entered is not a valid email
 Email entered does not belong to a user
 '''
 
+global auth_register.data{}
+
 def auth_login(email, password):
     
     if check_email(email) == "Invalid Email":
         raise ValueError("Invalid Email Address")
         
     #not a value password, example case 
-    if password != "Hello123":
-        raise ValueError("Incorrect Password")    
+    #if password != "Hello123":
+    #    raise ValueError("Incorrect Password")   
+
+    # find the user
+    for user in global auth_register.data{}:
+        if user['email'] == email:
+            if user['password'] == password:
+                # login and break
+                ret{}
+                ret['u_id'] = user['name_first'] + user['name_last']
+                ret['token'] = "#" #temp token
+                return ret
+            else:
+                # password doent match
+                # throw error
+        else:
+            pass
+
+    # if get to here, email isnt associated with an account
+    # return error, email not associated with a account
     
-    #return token for frontend
-    return "#"
+    
     
--- server/auth_register.py
+++ server/auth_register.py
@@ -1,5 +1,5 @@
 #definition auth_register function
-from email_check import *
+from email_check import email_check
 
 '''
 Given a user's first and last name, email address, and password, create a new account for them and return a new token for authentication in their session
@@ -12,28 +12,51 @@ name_first is more than 50 characters
 name_last is more than 50 characters
 '''
 
+# global variable
+global data
+data = {}
 
 def auth_register(email, password, name_first, name_last):
 
    #check if valid email
-   if email_check(email) == "Invalid Email":
-       raise ValueError("Invalid Email Address")
+    if email_check(email) == "Invalid Email":
+        raise ValueError("Invalid Email Address")
     
    #dummy case to check email already has a account created    
-   if email == "z5160026@unsw.edu.au":
-       raise ValueError("Email Already Exists")    
+   #if email == "z5160026@unsw.edu.au":
+   #   raise ValueError("Email Already Exists") 
+   #
+   # need to inplement this when perm data structure is decided   
     
    #password length check
-   if len(password) < 5:
-       raise ValueError("Invalid Password Length")
+    if len(password) < 5:
+        raise ValueError("Invalid Password Length")
         
    #first name length check    
-   if len(name_first) > 50 or len(name_first) == 0:
-       raise ValueError("Invalid First Name Length")
+    if len(name_first) > 50 or len(name_first) == 0:
+        raise ValueError("Invalid First Name Length")
     
    #last name length check   
-   if len(name_last) > 50 or len(name_last) == 0:
-       raise ValueError("Invalid Last Name Length")            
-   
-   #dummy return token and user id
-   return {0,"#"}
+    if len(name_last) > 50 or len(name_last) == 0:
+        raise ValueError("Invalid Last Name Length")
+
+
+   # add user dictionary to add to the global dictionary
+
+   # create new user to the data dictionary, need to check syntax
+    data[len(data)] = {}
+    data[len(data)]['name_first'] = name_first
+    data[len(data)]['name_last'] = name_last
+    data[len(data)]['password'] = password
+    data[len(data)]['email'] = email
+
+    #think about implementing a variable for u_id within data
+    #currently, two names may clash
+
+    # return a dictionary of u_id and token
+    ret = {}
+
+    ret['u_id'] = name_first + name_last
+    ret['token'] = "#" # temporary return token
+
+    return ret
