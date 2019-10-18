c65fc0d4bff67e3e1eaf0aff2dfceb3f512f2c50
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-18 15:16:01 +1100

    pseudocode implementation

--- server/search.py
+++ server/search.py
@@ -1,7 +1,27 @@
 #implementation of search function
+from json import dumps
+from flask import Flask,request
+import jwt
 
-'''
-Given a query string, return a collection of messages that match the query
-'''
-def search(token, query_str):
-    pass
+APP = Flask(__name__)
+
+
+@APP.route('/search', methods=['GET'])
+def search():
+    messages = {}
+
+    token = request.args.get('token')
+    query_str = request.args.get('query_str')
+
+    # get the dictionary to compare members of channel with user
+
+    # compare channels which the user is in
+
+        # look through the messages and compare with the query str
+
+        # if match, add to a dictionary
+
+
+    #return the dictionary of items associated with query string 
+    return dumps(messages)  
+
