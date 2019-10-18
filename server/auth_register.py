f2aa79bed14c363e5e90257349eeae305cac3a31
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-18 12:41:57 +1100

    working, need to check error

--- server/auth_register.py
+++ server/auth_register.py
@@ -1,5 +1,9 @@
 #definition auth_register function
 from email_check import email_check
+from flask import Flask, request
+from json import dumps
+import jwt
+import hashlib
 
 '''
 Given a user's first and last name, email address, and password, create a new account for them and return a new token for authentication in their session
@@ -12,51 +16,88 @@ name_first is more than 50 characters
 name_last is more than 50 characters
 '''
 
-# global variable
-global data
-data = {}
+APP = Flask(__name__)
 
-def auth_register(email, password, name_first, name_last):
+SECRET = 'COMP1531'
 
-   #check if valid email
+# global variable for users
+
+user = 0
+data = {
+    'users': {} 
+}
+
+def getData():
+    global data
+    return data
+
+def getSecret():
+    global SECRET
+    return SECRET    
+
+def getUsers():
+    global user
+    return user
+
+def incUser():
+    global user
+    user+=1        
+
+
+@APP.route('/auth/register', methods=['POST'])
+def auth_register():
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    name_first = request.form.get('name_first')
+    name_last = request.form.get('name_last')
+
+    #check if valid email
     if email_check(email) == "Invalid Email":
         raise ValueError("Invalid Email Address")
     
-   #dummy case to check email already has a account created    
-   #if email == "z5160026@unsw.edu.au":
-   #   raise ValueError("Email Already Exists") 
-   #
-   # need to inplement this when perm data structure is decided   
-    
-   #password length check
+    #password length check
     if len(password) < 5:
         raise ValueError("Invalid Password Length")
         
-   #first name length check    
+    #first name length check    
     if len(name_first) > 50 or len(name_first) == 0:
         raise ValueError("Invalid First Name Length")
     
-   #last name length check   
+    #last name length check   
     if len(name_last) > 50 or len(name_last) == 0:
         raise ValueError("Invalid Last Name Length")
 
 
-   # add user dictionary to add to the global dictionary
+    # add user dictionary to add to the global dictionary
 
-   # create new user to the data dictionary, need to check syntax
-    data[len(data)] = {}
-    data[len(data)]['name_first'] = name_first
-    data[len(data)]['name_last'] = name_last
-    data[len(data)]['password'] = password
-    data[len(data)]['email'] = email
+    # create new user to the data dictionary, need to check syntax
+    data = getData()
+    user = getUsers()
+    data['users'][user] = {'email': email, 'password': hashlib.sha256(password.encode()), 'name_first': name_first,
+     'name_last': name_last}
+    incUser()
+
+    #print(data)
 
     #think about implementing a variable for u_id within data
     #currently, two names may clash
 
     # return a dictionary of u_id and token
     ret = {}
+    u_id = name_first.lower() + name_last.lower()
+    u_id = ''.join(u_id)
 
-    ret['u_id'] = name_first + name_last
-    ret['token'] = "#" # temporary return token
+    # if the u_id is greater than 20 character, reduce
+    if len(u_id)>20:
+        u_id = u_id[0:19]
 
-    return ret
+    SECRET = getSecret()
+
+    ret['u_id'] = u_id
+    ret['token'] = jwt.encode({'u_id': ret['u_id']}, SECRET, algorithm='HS256').decode('utf-8')
+
+    return dumps(ret)
+
+if __name__ == '__main__':
+    APP.run(port=20000, debug=True)
