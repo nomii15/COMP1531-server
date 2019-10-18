3cde0f4be8f40c2ec2c59ea3167c907c8e62f6fe
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-18 14:58:52 +1100

    combined files into auth.py, need to fix problem

--- server/auth.py
+++ server/auth.py
@@ -4,19 +4,6 @@ from flask import Flask, request
 from json import dumps
 import jwt
 import hashlib
-import auth_login
-
-
-'''
-Given a user's first and last name, email address, and password, create a new account for them and return a new token for authentication in their session
-
-ValueError when:
-Email entered is not a valid email.
-Email address is already being used by another user
-Password entered is not a valid password
-name_first is more than 50 characters
-name_last is more than 50 characters
-'''
 
 APP = Flask(__name__)
 
@@ -100,9 +87,47 @@ def auth_register():
     return dumps(ret)
 
 @APP.route('/auth/login', methods=['POST'])
-def login():
-    
-    auth_login.auth_login()
+def auth_login():
+
+    email = request.form.get('email')    
+    password = request.form.get('password')
+
+    if email_check(email) == "Invalid Email":
+        raise ValueError("Invalid Email Address")
+        
+
+    # find the user
+    #data = getData()
+    #print(data['users'])
+    i = 0
+    data = getData()
+   # print("============")
+   # print(data)
+   # print("============")
+    for em, pas in data['users'].items():
+        #print(em)
+        #print(pas)
+        if pas['email'] == email:
+            if pas['password'].hexdigest() == hashlib.sha256(password.encode()).hexdigest():
+                # login and break
+                ret = {}
+                u_id = pas['name_first'].lower() + pas['name_last'].lower()
+                u_id = ''.join(u_id)
+                ret['u_id'] = u_id
+                # if the u_id is greater than 20 character, reduce
+                if len(u_id)>20:
+                    u_id = u_id[0:19]
+                ret['token'] = jwt.encode({'u_id': ret['u_id']}, SECRET, algorithm='HS256').decode('utf-8')
+                return dumps(ret)
+            else:
+                raise ValueError("Invalid Password")
+        else:
+            pass
+        i+=1    
+
+    raise ValueError("Incorrect Email address")
+    # if get to here, email isnt associated with an account
+    # return error, email not associated with a account
 
 if __name__ == '__main__':
     APP.run(port=20000, debug=True)
--- server/auth_login.py
+++ server/auth_login.py
@@ -1,8 +1,12 @@
 ##auth_login definition
-import re
 from email_check import email_check
 # import the function and the global dictionary
-import auth_register
+from auth_register import *
+from flask import Flask, request
+from json import dumps
+import jwt
+import hashlib
+
 
 '''
 Given a registered users' email and password and generates a valid token for the user to remain authenticated
@@ -12,32 +16,49 @@ Email entered is not a valid email
 Email entered does not belong to a user
 '''
 
-global auth_register.data{}
+APP = Flask(__name__)
 
-def auth_login(email, password):
+
+
+#@APP.route('/auth/login', methods=['POST'])
+def auth_login():
     
-    if check_email(email) == "Invalid Email":
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email_check(email) == "Invalid Email":
         raise ValueError("Invalid Email Address")
         
-    #not a value password, example case 
-    #if password != "Hello123":
-    #    raise ValueError("Incorrect Password")   
 
     # find the user
-    for user in global auth_register.data{}:
+    #data = getData()
+    #print(data['users'])
+    i = 0
+    data = getData()
+    print("============")
+    print(data)
+    print("============")
+    for user in data['users'][i]:
+        print(user)
         if user['email'] == email:
-            if user['password'] == password:
+            if user['password'] == hashlib.sha256(password.encode()):
                 # login and break
-                ret{}
-                ret['u_id'] = user['name_first'] + user['name_last']
-                ret['token'] = "#" #temp token
-                return ret
+                ret = {}
+                u_id = user['name_first'].lower() + user['name_last'].lower()
+                u_id = ''.join(u_id)
+
+                # if the u_id is greater than 20 character, reduce
+                if len(u_id)>20:
+                    u_id = u_id[0:19]
+                ret['token'] = jwt.encode({'u_id': ret['u_id']}, SECRET, algorithm='HS256').decode('utf-8')
+                return dumps(ret)
             else:
-                # password doent match
-                # throw error
+                raise ValueError("Invalid Password")
         else:
             pass
+        i+=1    
 
+    raise ValueError("Incorrect Email address")
     # if get to here, email isnt associated with an account
     # return error, email not associated with a account
     
