7153fb802b6d7b9e852ebea54ab77c6db755af12
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-19 17:48:05 +1100

    started implementing

--- server/auth_passwordreset_request.py
+++ server/auth_passwordreset_request.py
@@ -1,7 +1,15 @@
 #definition of the auth_passwordreset_request function
 
+#base for this function comes from the sample code provided (myemail.py)
+
+from flask import Flask
+from flask_mail import Mail, Message
+
 from email_check import email_check
-import auth_register 
+from data import *
+
+
+
 
 '''
 Given an email address, if the user is a registered user, send's them a an email containing a specific secret code, that when entered in auth_passwordreset_reset, shows that the user trying to reset the password is the one who got sent this email.
@@ -9,19 +17,46 @@ Given an email address, if the user is a registered user, send's them a an email
 
 N/A
 '''
-global code
 
-def auth_passwordreset_request(email):
+
+def auth_passwordreset_request():
+
+    email = request.form.get('email')
+
+    APP.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = 'DJMN1531@gmail.com',
+    MAIL_PASSWORD = "password1531"  
+                    )
+
+    mail = Mail(APP)                
+
     
     #invalid email entered
     if email_check(email) == "Invalid Email":
         raise ValueError("Invalid Email Address")
 
+    global data
+    data = getData()    
+
     # look through the data to see if the email is there
-    for Email in auth_register.data.items():
-        if Email['email'] == email:
+    for id, item in data['users'].items():
+        if item['email'] == email:
             # send the code to reset the password
-            # need to store the request to compare later
+            try:
+                msg = Message("Send Mail Test!",
+                    sender="DJMN1531@gmail.com",
+                    recipients=[item['email']])
+                # get a better reset code , maybe a random number generator   
+                msg.body = "123456"
+                mail.send(msg)
+                return dumps({})
+            except Exception as e:
+                return (str(e))        
+
+            
 
             # break
 
