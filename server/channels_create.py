f43c2d479c02c5157b09e0eb5cb1ea400a1391ed
Your Name <email@example.com>
2019-10-21 00:39:23 +1100

    minor edits required

--- server/channels_create.py
+++ server/channels_create.py
@@ -5,6 +5,22 @@ ValueError when:
     Name is more than 20 characters long
 
 '''
-
+# maybe need to change route - double check later
+@APP.route('channels/create', methods['POST'])
 def channels_create(token, name, is_public):
-    pass
+    # channel name length check
+    if len(name) > 20:
+        raise ValueError("Channel name too long")
+
+    #add channel dictionary to add to global dictionary
+    global data
+    data = getData()
+
+    global channel
+    channel = getChannels()
+
+    channel_id = channel
+
+    data['channels'][channel] = {'channel_id': channel_id, 'name': name}
+
+    return channel_id
