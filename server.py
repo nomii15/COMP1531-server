68576b406deb885a614eb510e757121ec0c495ec
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-20 15:03:09 +1100

    blueprints

--- server.py
+++ server.py
@@ -4,16 +4,23 @@ from flask_cors import CORS
 from json import dumps
 from flask import Flask, request
 
-sys.path.insert(0,'/tmp_amd/cage/export/cage/3/z5110036/comp1531/project/W15A-DJMN/server/')
-from auth_login import auth_login
-from auth_logout import auth_logout
-from auth_register import auth_register
 
+sys.path.insert(0,'/tmp_amd/cage/export/cage/3/z5110036/comp1531/project/W15A-DJMN/server/')
+from auth_login import login
+from auth_logout import logout
+from auth_register import register
 
 
 APP = Flask(__name__)
 CORS(APP)
-
+APP.register_blueprint(register)
+APP.register_blueprint(login)
+APP.register_blueprint(logout)
+'''
+@APP.route('/auth/register', methods=['POST'])
+def echo4():
+    pass
+'''
 @APP.route('/echo/get', methods=['GET'])
 def echo1():
     """ Description of function """
@@ -28,19 +35,7 @@ def echo2():
         'echo' : request.form.get('echo'),
     })
 
-@APP.route('/auth/register', methods=['POST'])
-def register():
-    return auth_register()
-
-@APP.route('/auth/login', methods=['POST'])
-def login():
-    return auth_login()
-
-@APP.route('/auth/logout', methods=['POST'])
-def logout():
-    return auth_logout()        
-
-
 if __name__ == '__main__':
     APP.run(port=(sys.argv[1] if len(sys.argv) > 1 else 5000))
+
 
