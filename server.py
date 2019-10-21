bf42177c6b92615435c1a87d5bcc0aff4946e55b
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-10-21 13:55:49 +1100

    add auth

--- server.py
+++ server.py
@@ -4,23 +4,25 @@ from flask_cors import CORS
 from json import dumps
 from flask import Flask, request
 
-
+'''
 sys.path.insert(0,'/tmp_amd/cage/export/cage/3/z5110036/comp1531/project/W15A-DJMN/server/')
 from auth_login import login
 from auth_logout import logout
 from auth_register import register
-
+'''
 
 APP = Flask(__name__)
 CORS(APP)
+'''
 APP.register_blueprint(register)
 APP.register_blueprint(login)
 APP.register_blueprint(logout)
 '''
+
 @APP.route('/auth/register', methods=['POST'])
 def echo4():
     pass
-'''
+
 @APP.route('/echo/get', methods=['GET'])
 def echo1():
     """ Description of function """
