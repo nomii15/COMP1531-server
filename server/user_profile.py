d062366c8a1edaff64b1329644a9a6943642e3a7
Junxuan Zhang <junxuan.zhang1@student.unsw.edu.au>
2019-10-26 18:10:07 +1100

    Updated

--- server/user_profile.py
+++ server/user_profile.py
@@ -1,25 +1,28 @@
 from flask import Flask, request, Blueprint
 from json import dumps
-import jwt
 from data import *
+from uid_check import *
 
-App = Flask(__name__)
+profile = Blueprint('profile', __name__)
 
-@App.route('/user/profile', methods=['GET'])
+@profile.route('/user/profile', methods=['GET'])
 def user_profile():
     data = getData()
     token = request.args.get('token')
     u_id = request.args.get('u_id')
-    email = None
-    for user in data['users']:
-        if user['u_id'] == u_id:
-            email= user['email']
-            name_first = user['name_first']
-            name_last = user['name_last']
-            #handle
+    
+    #for invalid u_id given
+    if uid_check(u_id) == False:
+        raise ValueError("invalid u_id.")
+    
+    #get the given user's detail
+    for key, item in data['users'].items():
+        if key == u_id:
+            email= item['email']
+            name_first = item['name_first']
+            name_last = item['name_last']
+            handle = item['handle']
             break
-    if email == None:
-        raise ValueError('invalid u_id')
     
     return dumps({
         'email' : email
