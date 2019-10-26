379a1feb602c1455611567b215329c9f33255b72
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-26 22:02:46 +1100

    pylint pass

--- server/user_profile_sethandle.py
+++ server/user_profile_sethandle.py
@@ -1,32 +1,36 @@
-from flask import Flask, request, Blueprint
 from json import dumps
+from flask import request, Blueprint
 import jwt
 from data import *
+from token_check import *
 
-sethandle = Blueprint('sethandle', __name__)
+SETHANDLE = Blueprint('SETHANDLE', __name__)
 
-@sethandle.route('/user/profile/sethandle', method=['PUT'])
+@SETHANDLE.route('/user/profile/sethandle', method=['PUT'])
 def user_profile_sethandle():
     data = getData()
     token = request.form.get('token')
     handle = request.form.get('handle_str')
-    
+    #check token is valid or not
+    if token_check(token) is not True:
+        return dumps({
+            'error' : 'invalid token'
+        })
     #the handle must in the range of 3-20
-    if len(handle)<3 or len(handle)>20:
+    if len(handle) < 3 or len(handle) > 20:
         raise ValueError("incorrect handle length")
-        
-    #if the handle is already been used, raise exception    
+    #if the handle is already been used, raise exception
     for key, item in data['users'].items():
         if item['handle'] == handle:
             raise ValueError("handle has been used")
-    
-    #decode the token and get u_id of the authorised user 
+    #decode the token and get u_id of the authorised user
     SECRET = getSecret()
-    Payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-    u_id = Payload['u_id']
-    
+    payload = jwt.decode(token, SECRET, algorithms=['HS256'])
+    u_id = payload['u_id']
     #update the handle
     for key, item in data['users'].items():
         if key == u_id:
             item['handle'] = handle
             break
+    return dumps({
+    })
