d3e499c909d19ea037a454a95f9ed78699beedf7
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-26 22:03:25 +1100

    no changes

--- server/channel_invite.py
+++ server/channel_invite.py
@@ -4,24 +4,50 @@ from data import *
 from uid_check import *
 from channel_check import *
 
-invite = Blueprint('invite', __name__)
+
+invite = Blueprint('invite',__name__)
+
 
 @invite.route('/channel/invite', methods=['POST'])
 def channel_invite():
+    global data
     data = getData()
     token = request.form.get('token')
+
     channel_id = int(request.form.get('channel_id'))
     invitee = request.form.get('u_id')
+
     
     #exceptions
     if member_check(token, channel_id) == False:
         raise AccessError("inviter is not a member of the given channel.")
         
-    if id_check(channel_id) == False:
-        raise ValueError("invalid channel ID.")
+
+    if id_check(int(channel_id)):
+        raise ValueError("channel_id does not refer to a valid channel that the authorised user is part of.")
+
         
-    if uid_check(invitee) == False:
+    if uid_check(int(u_id)) == False:
         raise ValueError("invalid u_id.")
 
-    #the invitee is added to the channel        
-    data['channel_details'][channel_id]['all_members'].append(invitee)
+
+    # value error when channel does not exist
+    for i, channel in data['channels'].items():
+        if channel['channel_id'] == int(channel_id):
+            # get dictionary of users details
+            for j, items in data['users'].items():
+                if u_id == items['u_id']:
+                    
+                    ret = {}
+                    ret['u_id'] = items['u_id']
+                    ret['name_first'] = items['name_first']
+                    ret['name_last'] = items['name_last']
+                    data['channels'][channel_id]['all_members'].append()
+                    return dumps({})
+            # if get to end of this loop, user isnt valid
+
+    # if here channel doesnt exist                
+
+
+
+
