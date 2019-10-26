7e8a02d27cd3ffae6c10941398229c9b258c56a5
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-26 19:08:29 +1100

    updated

--- server/channel_invite.py
+++ server/channel_invite.py
@@ -1,26 +1,27 @@
 from flask import Flask, request, Blueprint
 from json import dumps
-import jwt
 from data import *
 from uid_check import *
 from channel_check import *
 
-App = Flask(__name__)
+invite = Blueprint('invite', __name__)
 
-@App.route('/channel/invite', methods=['POST'])
+@invite.route('/channel/invite', methods=['POST'])
 def channel_invite():
     data = getData()
     token = request.form.get('token')
-    channel_id = request.form.get('channel_id')
+    channel_id = int(request.form.get('channel_id'))
     invitee = request.form.get('u_id')
     
+    #exceptions
     if member_check(token, channel_id) == False:
         raise AccessError("inviter is not a member of the given channel.")
         
-    if ...:
-        raise ValueError("channel_id does not refer to a valid channel that the authorised user is part of.")
+    if id_check(channel_id) == False:
+        raise ValueError("invalid channel ID.")
         
     if uid_check(invitee) == False:
         raise ValueError("invalid u_id.")
-        
-    data['channel_details']['channel_id']['all_members'].append(invitee)
+
+    #the invitee is added to the channel        
+    data['channel_details'][channel_id]['all_members'].append(invitee)
