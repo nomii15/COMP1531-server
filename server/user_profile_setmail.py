d84aa13f043f95d9267ee223f83f65409cdc498a
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-26 18:34:09 +1100

    updated

--- server/user_profile_setmail.py
+++ server/user_profile_setmail.py
@@ -2,28 +2,33 @@ from flask import Flask, request, Blueprint
 from json import dumps
 import jwt
 from data import *
-from email_check.py import email_check
+from email_check.py import *
 
-@App.route('/user/profile/setmail', method=['PUT'])
+setmail = Blueprint('setmail', __name__)
+
+@setmail.route('/user/profile/setmail', method=['PUT'])
 def user_profile_setmail():
     data = getData()
     token = request.form.get('token')
     email = request.form.get('email')
     
+    #if the email enter is not valid    
     if email_check(email) == "Invalid Email":
         raise ValueError("Invalid email address")
     
-    for user in data['users']:
-        if user['email'] == email:
+    #if the email address has already been used
+    for key, item in data['users'].items():
+        if item['email'] == email:
             raise ValueError("email address has been used")
-            
+                
+    #decode the token and get u_id of the authorised user       
     SECRET = getSecret()
-        
-    Payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-    
+    Payload = jwt.decode(token, SECRET, algorithms=['HS256'])    
     u_id = Payload['u_id']
     
-    for user in data['users']:
-        if user['u_id'] == u_id:
-            user['email'] = email
+    #update the email
+    for key, item in data['users'].items():
+        if key == u_id:
+            item['email'] = email
+            break
             
