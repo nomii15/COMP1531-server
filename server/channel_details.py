f05a0edd8f600bc697bbc9b9e6bde2355d7f993e
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-26 18:35:10 +1100

    updated

--- server/channel_details.py
+++ server/channel_details.py
@@ -5,21 +5,38 @@ from data import *
 from uid_check import *
 from channel_check import *
 
-App = Flask(__name__)
-
-App.route=('channel/details', methods=['GET'])
+details = Blueprint('details',__name__) 
+@details.route('/channel/details', methods=['GET'])
 def channel_details():
+
+    global data
+    data = getData()
+
     token = request.args.get('token')
-    channel_id = request.args.get('channel_id')
+    channel_id = int(request.args.get('channel_id'))
     
     if id_check(channel_id) == False:
         raise ValueError("invalid channel id")
-        
-    if member_check(token) == False:
+    #print("get here")  
+    if member_check(token, channel_id) == False:
         raise AccessError("Authorised user is not a member of this channel.")
-        
-    return dumps({
-        "name": data['channel_details'][channel_id]['name']
-        "owner_members": data['channel_details'][channel_id]['owner_members']
-        "all_members": data['channel_details'][channel_id]['all_members']
-    })
+    #print("get here 2")
+
+    ret = {
+        'name': data['channel_details'][channel_id]['name'],
+        'owner_members': [],
+        'all_members': []
+    }
+    
+    # get the names of the members
+    for i, item in data['users'].items():
+        if item['u_id'] in data['channel_details'][channel_id]['all_members']:
+            if item['u_id'] in data['channel_details'][channel_id]['owner_members']:
+                # add to owners
+                ret['owner_members'].append(  {'u_id': item['u_id'], 'name_first': item['name_first'], 'name_last': item['name_last'] })
+            else:
+                # add to all
+                ret['all_members'].append(  {'u_id': item['u_id'], 'name_first': item['name_first'], 'name_last': item['name_last'] })  
+    print(ret)
+    return dumps(ret)
+
