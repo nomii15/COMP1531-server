e033f0a80ed1b0afb901d954f246b2c990ba98f8
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-27 18:52:02 +1100

    finish

--- server/user_profile_test.py
+++ server/user_profile_test.py
@@ -1,26 +1,40 @@
 import pytest
-def test_user_profile_correct():
-    #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
-    token = authRegDic['token']
-    Uid = authRegDic['u_id']
-    #test part
-    UserProfileDic = user_profile(token, Uid)
-    assert(UserProfileDic['email']) == "Happy@gmail.com"
-    assert(UserProfileDic['name_first']) == "Happy"
-    assert(UserProfileDic['name_last']) == "Man"
-    assert(UserProfileDic['handle_str']) == "HappyMan"
-    
-def test_user_profile_invalid():
-    #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
-    token = authRegDic['token']
-    Uid = authRegDic['u_id']
-    #ensure Uid2 will not coincide with Uid
-    Uid2 = Uid + 1
-    #test part
-    with pytest.raises(ValueError,match=r"*"):
-        UserProfileDic = user_profile(token, Uid2)
         
+data = {
+    'users':{
+        '001':{'email': 'abcdefg@gmail.com', 'name_first': 'Tom', 'name_last': 'Happy', 'handle': 'tomhappy'},
+        '002':{'email': 'qwerty@gmail.com', 'name_first': 'Bob', 'name_last': 'Sad', 'handle': 'bobsad'}
+    }
+}
 
+class test_profile():
+    def __init__(self, token, u_id):
+        self.token = token
+        self.u_id = u_id
+        
+    def id_check(self):
+        for key, item in data['users'].items():
+            if key == self.u_id:
+                return True
+        raise ValueError("invalid u_id")
+            
+    def success(self):
+        return data['users'][self.u_id]
 
+#return information for user 001
+def test_1():
+    test1 = test_profile(12345, '001')
+    assert test1.id_check() == True
+    assert test1.success() == {'email': 'abcdefg@gmail.com', 'name_first': 'Tom', 'name_last': 'Happy', 'handle': 'tomhappy'}
+
+#return information for user 002
+def test_2():
+    test2 = test_profile(54321, '002')
+    assert test2.id_check() == True
+    assert test2.success() == {'email': 'qwerty@gmail.com', 'name_first': 'Bob', 'name_last': 'Sad', 'handle': 'bobsad'}
+    
+#invalid u_id
+def test_3():
+    test3 = test_profile(23451, '003')
+    with pytest.raises(ValueError, match='*invalid u_id*'):
+        test3.id_check()
