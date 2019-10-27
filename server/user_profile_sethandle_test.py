7e0172dd024a82ce9265cdafd75845e3eadfc1f8
Junxuan <z5221478@ad.unsw.edu.au>
2019-10-27 18:52:28 +1100

    finish

--- server/user_profile_sethandle_test.py
+++ server/user_profile_sethandle_test.py
@@ -1,19 +1,58 @@
 import pytest
+import jwt
+import re
 
-def test_user_profile_sethandle_correct():
-    #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
-    token = authRegDic['token']
-    Uid = authRegDic['u_id']
-    #test part            
-    user_profile_sethandle(token,"qwertyuiopasdfghjklzxcvbnm")
-    UserPorfileDic = user_profile(token, Uid)
-    assert(UserPorfileDic['handle_str'] == "qwertyuiopasdfghjklzxcvbnm"
+SECRET = 'COMP1531'
+
+data = {
+    'users':{
+        '001':{'email': 'abcdefg@gmail.com', 'name_first': 'Tom', 'name_last': 'Happy', 'handle': 'tomhappy'},
+        '002':{'email': 'qwerty@gmail.com', 'name_first': 'Bob', 'name_last': 'Sad', 'handle': 'bobsad'}
+    }
+}
+
+class test_handle():
+    def __init__(self, token, handle):
+        payload = jwt.decode(token, SECRET, algorithms=['HS256'])
+        self.u_id = payload['u_id']
+        self.handle = handle
+        
+    def handle_length(self):
+        if 3 <= len(self.handle) <= 20:
+            return True
+        raise ValueError("incorrect length")
+        
+    def handle_check(self):
+        for key, item in data['users'].items():
+            if item['handle'] == self.handle:
+                raise ValueError("handle been used")
+        return True
     
-def test_user_profile_sethandle_error():
-    #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
-    token = authRegDic['token']
-    #test part            
-    with pytest.raises(ValueError, match=r"*"):
-        user_profile_sethandle(token,"qwe")
+    def change(self):
+        data['users'][self.u_id]['handle'] = self.handle
+        
+    def new_data(self):
+        return data['users'][self.u_id]
+
+#success
+def test_1():
+    token = jwt.encode({'u_id': '001'}, SECRET, algorithm='HS256')
+    test1 = test_handle(token, 'abcdef')
+    assert test1.handle_length() == True
+    assert test1.handle_check() == True
+    test1.change()
+    assert test1.new_data() == {'email': 'abcdefg@gmail.com', 'name_first': 'Tom', 'name_last': 'Happy', 'handle': 'abcdef'}
+    
+#incorrect length
+def test_2():
+    token = jwt.encode({'u_id': '001'}, SECRET, algorithm='HS256')
+    test2 = test_name(token, 'a')
+    with pytest.raises(ValueError, match='*incorrect length*'):
+        test2.handle_length()
+
+#handle been used
+def test_3():
+    token = jwt.encode({'u_id': '001'}, SECRET, algorithm='HS256')
+    test3 = test_name(token, 'bobsad')
+    with pytest.raises(ValueError, match='*handle been used*'):
+        test3.handle_check()
