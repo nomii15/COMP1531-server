7b14ce4092d3392b0e844b331501a3f945314e5e
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-11-10 01:20:18 +1100

    started standup implementations, need to finish

--- server/standup_active.py
+++ server/standup_active.py
@@ -15,6 +15,28 @@ def standup_active(token, channel_id):
     # by default, is active is false and time finish is none when 
     # channel is created, only when standup start is 
     # called when these variables change
+    global data 
+    data = getData()
+
+    global standup
+    standup = getStandup()
+
+    # get the current time 
+    dt = datetime.utcnow()
+    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+    if timestamp > data['channels'][channel_id]['time_finish']:
+        #need to send the data from standup to the channel
+        stand_message = ""
+        for i in standup.items():
+            if i['channe_id'] == channel_id:
+                for j in i['messages']:
+                    #get the user and the message they sent
+                    stand_message = stand_message + j['name'] + ": " + j['message'] + "\n"
+                standup.remove(i) 
+                # have all the messages, delete standup and send message as person who started standup
+                message_send()
+
+
     return {    
         'is_active': data['channels'][channel_id]['standup_active'],
         'time_finish': data['channels'][channel_id]['time_finish']    
@@ -27,5 +49,5 @@ active = Blueprint('active', __name__)
 def route():
     token = request.args.get('token')
     channel_id = int(request.args.get('channel_id'))
-    print(token, channel_id)
+    #print(token, channel_id)
     return dumps( standup_active(token, channel_id) )
--- server/standup_start.py
+++ server/standup_start.py
@@ -1,4 +1,11 @@
 #implementation of function standup_start
+from data import *
+from flask import Flask, request, Blueprint
+from json import dumps
+from token_check import token_check
+from channel_check import id_check, member_check
+from Error import AccessError
+from datetime import timezone, datetime
 
 '''
 For a given channel, start the standup period whereby for the next 15 minutes if someone calls "standup_send" with a message, it is buffered during the 15 minute window then at the end of the 15 minute window a message will be added to the message queue in the channel from the user who started the standup.
@@ -7,5 +14,50 @@ ValueError when:Channel (based on ID) does not exist
 AccessError whenThe authorised user is not a member of the channel that the message is within
 '''
 
-def standup_start(token, channel_id):
-    pass
+def standup_start(token, channel_id, length):
+    
+    #check token
+    if token_check(token) == "Invalid_token":
+        raise ValueError("Invalid Token")
+
+    # check channel id
+    if id_check(channel_id)==False:
+        raise ValueError("Invalid Channel ID")
+
+    #check if member of channel
+    if member_check(token, channel_id) == False:
+        raise AccessError("Not a member of the channel")
+    
+    global data 
+    data = getData()    
+
+    # check whether a standup is already active
+    if data['channels'][channel_id]['standup_active'] == True:
+        raise ValueError("Standup already active")
+
+    # get the current time
+    dt = datetime.utcnow()
+    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+    #print(timestamp) 
+
+    global SECRET 
+    SECRET = getSecret()
+    token_payload = jwt.decode(token, SECRET, algorithms=['HS256'])
+    u_id = token_payload['u_id']  
+
+    # change the stantup variable to true and input the time
+    data['channels'][channel_id]['standup_active'] = True
+    data['channels'][channel_id]['startup_user'] = u_id
+    data['channels'][channel_id]['time_finish'] = timestamp + length
+    return data['channels'][channel_id]['time_finish']
+
+
+
+start = Blueprint('start', __name__)
+@start.route('/standup/start', methods=['POST'])
+def start_route():
+    token = request.form.get('token')
+    channel_id = int(request.form.get('channel_id'))
+    length = int(request.form.get('length'))
+    return dumps( standup_start(token, channel_id, length)  )
+
