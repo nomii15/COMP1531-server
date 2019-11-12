1fbc734371c6ce3bc0915e7d4819dbcc70b44061
Your Name <email@example.com>
2019-11-12 19:47:01 +1100

    fixed routes for channel functions

--- server/channel_addowner.py
+++ server/channel_addowner.py
@@ -20,26 +20,27 @@ from data import *
 
 addowner = Blueprint('addowner', __name__)
 @addowner.route('/channel/addowner')
-def channel_addowner(token, channel_id, u_id):
-    
+def Addowner():
     token = request.form.get('token')
     channel_id = request.form.get('channel_id')
     u_id = request.form.get('u_id')
+    return dumps(channel_addowner(token, channel_id, u_id))
 
+def channel_addowner(token, channel_id, u_id):
     if token_check(token) == False:
         raise AccessError('Invalid Token')
 
      #exceptions
     if member_check(token, channel_id) == False:
-        raise AccessError("inviter is not a member of the given channel.")
+        raise AccessError("Inviter is not a member of the given channel.")
         
 
     if id_check(int(channel_id)):
-        raise ValueError("channel_id does not refer to a valid channel that the authorised user is part of.")
+        raise ValueError("Channel_id does not refer to a valid channel that the authorised user is part of.")
 
         
     if uid_check(int(u_id)) == False:
-        raise ValueError("invalid u_id.")
+        raise ValueError("Invalid u_id.")
 
     # all valid, add u_id to owner channel
     for i, channel in data['channels'].items():
@@ -53,4 +54,4 @@ def channel_addowner(token, channel_id, u_id):
                     ret['name_first'] = items['name_first']
                     ret['name_last'] = items['name_last']
                     data['channels'][channel_id]['owner_members'].append()
-                    return dumps({})    
+                    return ret    
--- server/channel_details.py
+++ server/channel_details.py
@@ -7,11 +7,15 @@ from channel_check import *
 
 DETAILS = Blueprint('DETAILS', __name__)
 @DETAILS.route('/channel/details', methods=['GET'])
-def channel_details():
-    global data
-    data = getData()
+def Details():
     token = request.args.get('token')
     channel_id = int(request.args.get('channel_id'))
+    return dumps(channel_details(token, channel_id))
+
+def channel_details(token, channel_id):
+    global data
+    data = getData()
+
     #exceptions
     if id_check(channel_id) is not True:
         raise ValueError("invalid channel id")
@@ -30,4 +34,4 @@ def channel_details():
         for hold in data['channel_details'][channel_id]['all_members']:
             if item['u_id'] == hold['u_id']:
                 ret['all_members'].append({'u_id': item['u_id'], 'name_first': item['name_first'], 'name_last': item['name_last']})
-    return dumps(ret)
+    return ret
