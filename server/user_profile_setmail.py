ad5a16b16d0cc76403f890a3f5bb4b2dfd402213
Junxuan <z5221478@ad.unsw.edu.au>
2019-11-14 16:13:01 +1100

    frontend and test done

--- server/user_profile_setmail.py
+++ server/user_profile_setmail.py
@@ -7,24 +7,20 @@ from token_check import token_check
 
 SETMAIL = Blueprint('SETMAIL', __name__)
 
-@SETMAIL.route('/user/profile/setemail', methods=['PUT'])
-def user_profile_setmail():
-    data = getData()
-    token = request.form.get('token')
-    email = request.form.get('email')
+def user_profile_setmail(token, email):
+    data = getData()    
     #check token is valid or not
     if token_check(token) is not True:
-        return dumps({
+        return {
             'error' : 'invalid token'
-        })
+        }
     #if the email enter is not valid
     if email_check(email) == "Invalid Email":
-        raise ValueError("Invalid email address")
+        raise ValueError(description = "Invalid email address")
     #if the email address has already been used
     for key, item in data['users'].items():
         if item['email'] == email:
-            raise ValueError("email address has been used")
-            
+            raise ValueError(description = "email address has been used")            
     #decode the token and get u_id of the authorised user
     SECRET = getSecret()
     payload = jwt.decode(token, SECRET, algorithms=['HS256'])
@@ -34,5 +30,11 @@ def user_profile_setmail():
         if key == u_id:
             item['email'] = email
             break
-    return dumps({
-    })
+    return {
+    }
+
+@SETMAIL.route('/user/profile/setemail', methods=['PUT'])    
+def user_profile_setmail_route():
+    token = request.form.get('token')
+    email = request.form.get('email')
+    return dumps(user_profile_setmail(token, email))
