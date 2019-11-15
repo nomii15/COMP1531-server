84587b55ee9e889cdd04e908118939908acc8561
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-11-15 14:48:51 +1100

    implemented search functions and tests

--- server/search.py
+++ server/search.py
@@ -4,38 +4,43 @@ from flask import Flask,request, Blueprint
 import jwt
 
 from token_check import token_check
+from token_to_uid import token_to_uid
 from data import *
 
-search = Blueprint('search', __name__)
-
-
-@search.route('/search', methods=['GET'])
-def search():
-    ret = {'messages': []}
-
-    token = request.args.get('token')
-    query_str = request.args.get('query_str')
+def Search_function(token, query_str):
 
     if token_check(token) == False:
-        raise ValueError("Invalid token")
+        raise ValueError(description = "Invalid token")
 
-    global SECRET
-    SECRET = getSecret()
-    Payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-    u_id = Payload['u_id'] 
+    if len(query_str) > 1000:
+        raise ValueError(description = "Message is greater than 1000 characters")    
+
+    u_id = token_to_uid(token)
 
     global data
     data = getData()
 
+    ret = {'messages': []}
     # for all channels, if the user is part of that channels
     # get the messages that 
     for i, items in data['channel_details'].items():
         for j in items['all_members']:
-            if j == u_id:
+            #if they are apart of the channel, get messages
+            if j['u_id'] == u_id:
                 # the user is part of that channel
                 # see if query exists
-                if query_str in items['messages']:
-                    ret['messages'].append(items['messages'])
+                for mess in data['channels'][ items['channel_id'] ]['messages']:
+                    # see if the strings are in the messages
+                    if query_str in mess['message']:
+                        ret['messages'].append(mess)
 
-    return dumps(ret)            
+    return ret 
+
+
+Search_route = Blueprint('Search', __name__)
+@Search_route.route('/search', methods=['GET'])
+def Search_flask_route():
+    token = request.args.get('token')
+    query_str = str(request.args.get('query_str'))
+    return dumps( Search_function(token, query_str) )
 
