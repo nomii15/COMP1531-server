d69d55e1dc80bef9213841ea51b800c39736dcfa
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-11-16 17:49:54 +1100

    written function and tests work except correct

--- server/user_profile_uploadphoto_test.py
+++ server/user_profile_uploadphoto_test.py
@@ -1,32 +1,40 @@
 import pytest
+from auth_register import auth_register
+from user_profiles_uploadphoto import users_profiles_uploadphoto
+from data import *
 
+'''
 def test_user_profile_uploadphoto_correct():
     #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
+    authRegDic = auth_register("Hapcpy@gmail.com","qwe123","Happy","Man")
     token = authRegDic['token']
-    
+    url = "http://www.news.tracanada.ca/wp-content/uploads/2017/07/curata__fhH3JcHUGmZPaVe.jpeg"
     #test part
     #assume x_start, y_start, x_end, y_end are all within the dimensions
-    user_profiles_uploadphoto(token, 200, 100, 100, 100, 100)
-    
-    
+    users_profiles_uploadphoto(token, url, 0,0,100,100)
+    assert( data['users'][1]['img_profile_url'] != None )
+'''
     
 def test_user_profile_uploadphoto_error1():
     #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
+    authRegDic = auth_register("Happy@gmail.com","qwe123","Happy","Man")
     token = authRegDic['token']
+    url = 'http://www.news.tracanada.ca/wp-content/uploads/2017/07/curata__fhH3JcHUGmZPaVe.jpeg'
 
     #test part
     #img_url is return an HTTP status other than 200
     with pytest.raises(ValueError, match=r"*"):
-        user_profiles_uploadphoto(token, 300, 100, 100, 100, 100)
+        users_profiles_uploadphoto(token, "somefakeimage.png", 300, 100, 100, 100)
         
 def test_user_profile_uploadphoto_error2():
     #setup part
-    authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
+    #authRegDict = auth_register("Happy@gmail.com","qwe123","Happy","Man")
+    #token = authRegDic['token']
+    url = "http://www.news.tracanada.ca/wp-content/uploads/2017/07/curata__fhH3JcHUGmZPaVe.jpeg"
+    authRegDic = auth_register("Hfappy@gmail.com","qwe123","Happy","Man")
     token = authRegDic['token']
 
     #test part
     #x_start, y_start, x_end, y_end are out of the dimensions
     with pytest.raises(ValueError, match=r"*"):
-        user_profiles_uploadphoto(token, 200, 9999999999, 9999999999, 9999999999, 9999999999)
+        users_profiles_uploadphoto(token, url, 9999999999, 9999999999, 9999999999, 9999999999)
--- server/user_profiles_uploadphoto.py
+++ server/user_profiles_uploadphoto.py
@@ -5,32 +5,57 @@ from json import dumps
 import urllib.request
 import sys
 from PIL import Image
+import imghdr
+from token_to_uid import token_to_uid
+from token_check import token_check
 
 def users_profiles_uploadphoto(token, img_url, x_start, x_end, y_start, y_end):
     
-    # retrieve u_id from token
-    global SECRET 
-    SECRET = getSecret()
-    token_payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-    u_id = token_payload['u_id']
+    #error handling for inputs
+    if token_check(token) == False:
+        raise ValueError(descripton = "Invalid token")    
 
+    #error for image not being a jpeg image
+    # retrieve u_id from token
+    u_id = token_to_uid(token)
     port = sys.argv[1]
     url = f"http://127.0.0.1:{port}/static/"
-    name = f"{u_id}.png"
+    name = f"./static/{u_id}.jpeg"
+    try:
+        urllib.request.urlretrieve(img_url, name)
+    except Exception as e:
+        if type(e) != 200:
+            raise ValueError(description = "HTTP status not 200")
+        elif imghdr.what(name) != 'jpeg':
+            raise ValueError(description = "Image not a jpeg image")
+        else:
+            pass
+
+
     # add checks to see if that values are correct inputs
-    urllib.request.urlretrieve(img_url, "./static/"+name)
 
     #crop the url image
-    imageobject = Image.open("./static/"+name)
-    cropped = imageobject.crop((x_start,x_end,y_start,y_end))
-    cropped.save("./static/"+name)
+    imageobject = Image.open(name)
+
+    #check the dimensions of the image
+    width, height = imageobject.size
+    print(width)
+    print(height)
+    if x_end - x_start > width:
+        raise ValueError(description = "invalid x coordinates")
+    if y_end - y_start > height:
+        raise ValueError(description = "invalid y coordinates")    
+
+
+    cropped = imageobject.crop( (x_start,y_start,x_end,y_end) )
+    cropped.save(name)
 
     #print("get here")
 
     #save img url in users details
     for i, items in data['users'].items():
         if items['u_id'] == u_id:
-            items['profile_img_url'] = url + name
+            items['profile_img_url'] = url + f"{u_id}.jpeg"
             #print(items['profile_img_url'])
             break
 
