505da01767c8ad706c2abf35823eb32299e79fd5
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-11-17 12:36:56 +1100

    done tests

--- server/standup_start.py
+++ server/standup_start.py
@@ -4,10 +4,10 @@ from flask import Flask, request, Blueprint
 from json import dumps
 from token_check import token_check
 from channel_check import id_check, member_check
-from Error import AccessError
 from datetime import timezone, datetime
+from token_to_uid import token_to_uid
 import jwt
-
+import calendar
 '''
 For a given channel, start the standup period whereby for the next 15 minutes if someone calls "standup_send" with a message, it is buffered during the 15 minute window then at the end of the 15 minute window a message will be added to the message queue in the channel from the user who started the standup.
 
@@ -18,40 +18,43 @@ AccessError whenThe authorised user is not a member of the channel that the mess
 def standup_start(token, channel_id, length):
     
     #check token
-    if token_check(token) == "Invalid_token":
-        raise ValueError("Invalid Token")
+    if token_check(token) == False:
+        raise ValueError(description = "Invalid Token")
 
     # check channel id
     if id_check(channel_id)==False:
-        raise ValueError("Invalid Channel ID")
+        raise ValueError(description = "Invalid Channel ID")
 
     #check if member of channel
     if member_check(token, channel_id) == False:
-        raise AccessError("Not a member of the channel")
+        raise AccessError(description = "Not a member of the channel")
     
     global data 
     data = getData()    
 
     # check whether a standup is already active
     if data['channels'][channel_id]['standup_active'] == True:
-        raise ValueError("Standup already active")
+        raise ValueError(description = "Standup already active")
 
     # get the current time
-    dt = datetime.utcnow()
-    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+    #dt = datetime.now().timestanp
+
+    d = datetime.utcnow()
+    unixtime = calendar.timegm(d.utctimetuple())
+    #print (unixtime)
+    #dt = time.time()
+    #dt = datetime.utcnow()
+    #timestamp = int(dt.replace(tzinfo=timezone.utc).timestamp())
     #print(timestamp) 
 
-    global SECRET 
-    SECRET = getSecret()
-    token_payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-    u_id = token_payload['u_id']  
+    u_id = token_to_uid(token) 
 
     # change the stantup variable to true and input the time
     data['channels'][channel_id]['standup_active'] = True
     data['channels'][channel_id]['startup_user'] = u_id
-    data['channels'][channel_id]['time_finish'] = timestamp + length
-    return data['channels'][channel_id]['time_finish']
-
+    data['channels'][channel_id]['time_finish'] = unixtime + length
+    ret = {'time_finish': data['channels'][channel_id]['time_finish']}
+    return ret
 
 
 start = Blueprint('start', __name__)
--- server/standup_start_test.py
+++ server/standup_start_test.py
@@ -1,4 +1,11 @@
 from standup_start import standup_start
+from standup_active import standup_active
+from auth_register import auth_register
+from channels_create import channels_create
+from message_send import message_send
+from data import *
+import jwt
+import time
 import pytest
 
 #working setup
@@ -13,17 +20,15 @@ def test_standup_start1():
     channel = channels_create(token, "name", True)
     Id = channel['channel_id']
     
-    #get the current time
-    curr = datetime(2019,10,4,14,0)
-    
     #send a message
-    message_send(token, Id, "standup_send")
+    message_send(token, Id, "/standup 10")
     
     #this should return the time in the form of datetime (for 15 min duration)
-    time = standup_start(token, Id)
+    active = standup_active(token, Id)
+    timer = standup_start(token, Id, 1)
+
+    assert data['channels'][Id]['standup_active'] == True
     
-    #this test should pass as the time duration is 15 min
-    assert time == (curr + time(14,15))
     
     
     
@@ -32,7 +37,7 @@ def test_standup_start2():
     
     
     #generate a token
-    test_login = auth_register("z5110036@unsw.edu.au", "1234567", "John", "Smith")
+    test_login = auth_register("z5110066@unsw.edu.au", "1234567", "John", "Smith")
     
     token = test_login['token']
     
@@ -47,45 +52,50 @@ def test_standup_start2():
     #this should fail as an invalid channel id is passed
     #assume the channel id 13 doesnt match variable Id
     with pytest.raises(ValueError, match = '*Invalid Channel ID*'):
-        standup_start("#11", 13)
+        standup_start(token, 13, 20)
         
         
 #invalid token
 def test_standup_start3():
 
     #generate a token
-    test_login = auth_register("z5110036@unsw.edu.au", "1234567", "John", "Smith")
+    test_login = auth_register("z5210036@unsw.edu.au", "1234567", "John", "Smith")
    
     token = test_login['token']
     
     #generate a channel
     channel = channels_create(token, "name", True)
     Id = channel['channel_id']
+
+    #generate a fake token
+    global SECRET
+    SECRET = getSecret()
+    false = jwt.encode({'u_id':-1}, SECRET, algorithm='HS256').decode('utf-8')
     
     #send a message
     message_send(token, Id, "standup_send")
     
     #assume invalid token, such that token variable above is not the 
     #same as the token being passed in
-    with pytest.raises(ValueError, match = '*Invalid Token*'):
-        standup_start("#72", 3)
+    with pytest.raises(ValueError, match = 'Invalid Token'):
+        standup_start(false, 1, 15)
         
 #not an authorised user
 def test_standup_start4():
     
     #generate a token
-    test_login = auth_register("z5110036@unsw.edu.au", "1234567", "John", "Smith")
+    test_login = auth_register("z5130036@unsw.edu.au", "1234567", "John", "Smith")
    
     token = test_login['token']
     
     #assume the channel is created and the user isnt a member
     
     #send a message
-    message_send(token, Id, "standup_send")
+    #message_send(token, Id, "standup_send")
     
     #this should fail as the user us not a member of the channel
     with pytest.raises(AccessError, match = '*Not a Member of the Channel*'):
-        standup_start(token, 4)
+        standup_start(token, 1, 20)
     
     
     
