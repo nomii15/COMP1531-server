c57175080637fe83f0b063897243b36d736f7e46
normorad <normoradnadir@gmail.com>
2019-11-17 15:40:01 +1100

    done sendlater tests

--- server/message_sendlater_test.py
+++ server/message_sendlater_test.py
@@ -2,71 +2,90 @@ from message_sendlater import message_sendlater
 from channels_create import channels_create
 from auth_register import auth_register
 from datetime import datetime
+from channel_join import channel_join
 from data import *
 import pytest
 
 #test for invalid channel_id
 def test_invalid_channel():
-    user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
-    token1 = user['token']
-
-    channelResponse = channels_create(token1, "My Channel", False)
-    channel_id = int(channelResponse['channel_id']) + 1
-
-    message = "A valid message"
+	owner = auth_register("validemail1@gmail.com", "validpassword1", "OWNER1", "validname1")
+	owner_token = owner['token']
+	user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
+	user1_token = user['token']
+		
+	channelResponse = channels_create(owner_token, "My Channel", True)
+	channel_id = int(channelResponse['channel_id'])
 	
-	now = datetime.now()
-    time_sent = int(datetime.timestamp(now)) + 100
+	channel_join(user1_token, channel_id)	
 
-    with pytest.raises(ValueError, match = '*Invalid Channel ID*'):
-        message_sendlater(token1, channel_id, message, time_sent)
+	message = "A valid message"
+
+	now = datetime.now()
+	time_sent = int(datetime.timestamp(now)) + 100
+
+	with pytest.raises(ValueError, match = '*Invalid Channel ID*'):
+		message_sendlater(user1_token, channel_id + 1, message, time_sent)
 
 #test for invalid message
 def test_invalid_message():
+	owner = auth_register("validemail1@gmail.com", "validpassword1", "OWNER1", "validname1")
+	owner_token = owner['token']
 	user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
-	token1 = user['token']
-	
-	channelResponse = channels_create(token1, "My Channel", False)
+	user1_token = user['token']
+		
+	channelResponse = channels_create(owner_token, "My Channel", True)
 	channel_id = int(channelResponse['channel_id'])
+	
+	channel_join(user1_token, channel_id)	
 	
 	message = "This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long  This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long This is an invalid message because it is too long  "
 	
 	now = datetime.now()
 	time_sent = int(datetime.timestamp(now)) + 100
 	
-	with pytest.raises(ValueError, match = '*The message you are trying to send is too large*'):
-		message_sendlater(token1, channel_id, message, time_sent)
+	with pytest.raises(ValueError, match = '*Invalid Message*'):
+		message_sendlater(user1_token, channel_id, message, time_sent)
 
 #test for invalid time
 def test_invalid_time():
-    user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
-    token1 = user['token']
-
-    channelResponse = channels_create(token1, "My Channel", False)
-    channel_id = int(channelResponse['channel_id'])
-
-    message = "A valid message"
-
-	now = datetime.now()
-    time_sent = int(datetime.timestamp(now)) - 100
-
-    with pytest.raises(ValueError, match = '*The time you selected is in the past*'):
-        message_sendlater(token1, channel_id, message, time_sent)
-    
-#valid cases
-def test_valid():
+	owner = auth_register("validemail1@gmail.com", "validpassword1", "OWNER1", "validname1")
+	owner_token = owner['token']
 	user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
-	token1 = user['token']
-
-	channelResponse = channels_create(token1, "My Channel", False)
+	user1_token = user['token']
+		
+	channelResponse = channels_create(owner_token, "My Channel", True)
 	channel_id = int(channelResponse['channel_id'])
+
+	channel_join(user1_token, channel_id)
 
 	message = "A valid message"
 
 	now = datetime.now()
-	time_sent = int(datetime.timestamp(now)) + 100
-		
-	global Message
-	message_id = getMessage()
+	time_sent = int(datetime.timestamp(now)) - 100
 
-	assert (message_sendlater(token1, channel_id, message, time_sent) == {'message_id': message_id})
+	with pytest.raises(ValueError, match = '*Invalid Time*'):
+		message_sendlater(user1_token, channel_id, message, time_sent)
+    
+#valid cases
+def test_valid():
+	owner = auth_register("validemail1@gmail.com", "validpassword1", "OWNER1", "validname1")
+	owner_token = owner['token']
+	user = auth_register("validemail1@gmail.com", "validpassword1", "validname", "validname")
+	user1_token = user['token']
+		
+	channelResponse = channels_create(owner_token, "My Channel", True)
+	channel_id = int(channelResponse['channel_id'])
+
+	channel_join(user1_token, channel_id)
+
+	now = datetime.now()
+	time_sent = int(datetime.timestamp(now)) + 100
+
+	global Message
+	expected_message_id = getMessage()
+
+	message = "A valid message"
+	message_sent = message_sendlater(user1_token, channel_id, message, time_sent)
+	message_id = message_sent['message_id']
+
+	assert message_id == expected_message_id
