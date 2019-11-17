d1cdba274bcd29b8b79ee539ece9c3654f3bc457
Daniel Setkiewicz <z5110036@unsw.edu.au>
2019-11-17 16:32:26 +1100

    testing and fixed error in indexing messages

--- server/channel_messages.py
+++ server/channel_messages.py
@@ -15,10 +15,10 @@ def channel_messages(token, channel_id, start):
     data = getData()
 
     if token_check(token) == False:
-        raise ValueError("Invalid token")
+        raise ValueError(description = "Invalid token")
 
     if member_check(token, channel_id) == False:
-        AccessError("Not a member")
+        raise AccessError(description = "Not a member")
 
     # return dictionary of the messages found, with initialisation
     message = {
@@ -39,14 +39,16 @@ def channel_messages(token, channel_id, start):
             
             #check if valid start point to read messages
             if start > len(data['channels'][channel_id]['messages']):
-                raise ValueError("Start greater than number of messages")
+                raise ValueError(description = "Start greater than number of messages")
 
-            if len(message['messages']) < 50:
+            if len(data['channels'][channel_id]['messages']) > 50 and start==0:
+                message['end'] = 49
+                end = len(data['channels'][channel_id]['messages'])
+
+            elif (len(data['channels'][channel_id]['messages']) - start) < 50:    
                 message['end'] = -1
-                end = len(message['messages']) - start
-            else:    
-                message['end'] = start+50
-                end = start+50
+                end = -1
+                #end = len(data['channels'][channel_id]['messages'])
             
             for items in data['channels'][channel_id]['messages']:
                 message['messages'].append(items)
@@ -71,7 +73,7 @@ def channel_messages(token, channel_id, start):
 
 
     # no match for a channel
-    raise ValueError("No channel matches the channel id given")
+    raise ValueError(description = "No channel matches the channel id given")
 
 
     # check that the channel id is correct
@@ -89,7 +91,7 @@ channel_message = Blueprint('channel_message', __name__)
 @channel_message.route('/channel/messages', methods = ['GET'])
 def getMessages():
 
-    token = request.args.get('token')
+    token = str(request.args.get('token'))
     channel_id = int(request.args.get('channel_id'))
     start = int(request.args.get('start'))
     return dumps( channel_messages(token, channel_id, start)     )
--- server/channel_messages_test.py
+++ server/channel_messages_test.py
@@ -1,86 +1,79 @@
 import pytest
-from auth_register_test import testRegister
-from Error import *
+from auth_register import auth_register
+from channels_create import channels_create
+from message_send import message_send
+from channel_messages import channel_messages
 
-class channel():
-    def __init__(self, i_d, name):
-        self.id = i_d
-        self.name = name
-        self.users = [0]
+from data import *
 
-    def checkID(self, code):
-        if self.id != code:
-            raise ValueError("Invalid Channel ID")
-    def read(self, start, end):
-        if start > end:
-            raise ValueError("Invalid Channel ID")  
-    def messages(self,user):
-        if user not in self.users:
-            raise AccessError("not a member")
 
-'''
 def test_channel_messages_correct():
     #setup part
     authRegDict1 = auth_register("Happy@gmail.com","qwe123","Happy","Man")
-    token1 = authRegDic1['token']
-    Uid1 = authRegDic1['u_id']
+    token1 = authRegDict1['token']
+    Uid1 = authRegDict1['u_id']
     
-    authRegDict2 = auth_register("Sad@gmail.com","qwe123","Sad","Woman")
-    Uid2 = authRegDic2['u_id']
+    Channelid = channels_create(token1, "name1", True)
     
-    Channelid = channels_create(token1, channel1, True)
-    channel_invite(token1, Channelid, Uid2)
-    
-    for i in range(75):
-        message_send(token1, Channelid, "hello")
+  
+    message_send(token1, Channelid['channel_id'], "hello")
     
     #test part    
-    MessageDic = channel_messages(token1, Channelid, 0)
+    MessageDic = channel_messages(token1, Channelid['channel_id'], 0)
     assert(MessageDic['start']) == 0
-    assert(MessageDic['end']) == 49
-    
-    MessageDic = channel_messages(token1, Channelid, 50)
-    assert(MessageDic['start']) == 50
     assert(MessageDic['end']) == -1
-'''
+
+    for i in range(0,45):
+        message_send(token1, Channelid['channel_id'], "hello")
+
+
+    MessageDic = channel_messages(token1, Channelid['channel_id'], 40)
+    assert(MessageDic['start']) == 40
+    assert(MessageDic['end']) == -1
+   
+
+
 def test_channel_messages_valueerror1():
 
     #register
-    test = testRegister("z5110036@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
+    test = auth_register("z5110046@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
 
+    token1 = test['token']
     #create a channel
-    chan = channel(0, "channel_name") 
+    chan = Channelid = channels_create(token1, "name1", True) 
        
     #test part
     #invalid channel id
     with pytest.raises(ValueError, match="Invalid Channel ID"):
-        chan.checkID(1)
+        channel_messages(token1, 50,0)
         
 def test_channel_message_valueerror2():
     #setup part
     #register
-    test = testRegister("z5110036@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
-
+    test = auth_register("z5110056@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
+    token1 = test['token']
     #create a channel
-    chan = channel(0, "channel_name") 
+    chan = channels_create(token1, "name11", True)  
        
 
     #test part
     #start is greater than the total number of messages in the channel
     with pytest.raises(ValueError, match=r"*"):
-        chan.read(40,20)
+        channel_messages(token1,chan['channel_id'] ,5)
         
 def test_channel_message_accesserror():
     #setup part
-    test1 = testRegister("z5110036@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
-    info1 = test1.valid()
-    
-    test2 = testRegister("hello@gmail.com.au", "SomePassword", "hello", "there")
-    info2 = test2.valid()
-    
-    chan = channel(0, "channel_name")
+    test1 = auth_register("z5110096@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
+    token1 = test1['token']
+
+    test2 = auth_register("z5110086@unsw.edu.au", "SomePassword", "Daniel", "Setkiewicz")
+    token2 = test2['token']
+
+
+    #create a channel
+    chan = channels_create(token1, "name11", True)     
     
     #test part
     #token1 user is not a member of channel with Channelid
     with pytest.raises(AccessError, match=r"*"):
-        chan.messages(1)
+        channel_messages(token2,chan['channel_id'], 0)
