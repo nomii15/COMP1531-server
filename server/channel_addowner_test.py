888f1c763c7c90d6ec047226d46ae1e76cf88e16
Your Name <email@example.com>
2019-11-17 14:46:35 +1100

    check register email error

--- server/channel_addowner_test.py
+++ server/channel_addowner_test.py
@@ -15,45 +15,36 @@ AccessError when:
     The authorised user is not an owner of the slackr, or an owner of this channel
 
 '''
+def setup():
+    #global data
+    #data = {}
 
-def test_value_error_channel():
-    #setup
-    register1 = auth_register("hello@gmail.com","abcde","hi","hello")
+    register1 = auth_register("validemail1@gmail.com", "validpassword1", "USER1", "validname1")
     token1 = register1['token']
     u_id1 = register1['u_id']
 
+    register2 = auth_register("validemail2@gmail.com", "validpassword2", "USER2", "validname2")
+    token2 = register2['token']
+    u_id2 = register2['u_id']
+
+    register3 = auth_register("validemail3@gmail.com", "validpassword3", "USER3", "validname3")
+    token3 = register3['token']
+    u_id3 = register3['u_id']
+
+    channel_id1 = channels_create(token1, 'channel1', True)
+    channel_id3 = channels_create(token3, 'channel3', True)
+
+def test_value_error_channel():
     #channel id does not exist
     with pytest.raises(ValueError,match=r"*"):
         channel_addowner(token1,1,u_id1)
 
 def test_value_error_owner():
-    #setup
-    register1 = auth_register("hello@gmail.com","abcde","hi","hello")
-    token1 = register1['token']
-    u_id1 = register1['u_id']
-
-    channel_id1 = channels_create(token1, channel1, True)
-    
     #user is already an owner of the channel
     with pytest.raises(ValueError,match=r"*"):
         channel_addowner(token1,channel_id1,u_id1)
 
 def test_access_error():
-    #setup
-    register1 = auth_register("hello@gmail.com","abcde","hi","hello")
-    token1 = register1['token']
-    u_id1 = register1['u_id']
-
-    register2 = auth_register("helloo@gmail.com","abcdef","hihi","helloo")
-    token2 = register2['token']
-    u_id2 = register2['u_id']
-
-    register3 = auth_register("hellooo@gmail.com","abcdefg","hihihi","hellooo")
-    token3 = register3['token']
-    u_id3 = register3['u_id']
-
-    channel_id3 = channels_create(token3, channel3, True)
-    
     #authorised user is not owner of slackr or channel
     with pytest.raises(ValueError,match=r"*"):
         channel_addowner(token2,channel_id3,u_id1)
